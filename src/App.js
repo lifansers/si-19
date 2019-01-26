99939dd7f0cb71e1c61cf41717ff596c44881776
lifansers <l.long14@pitt.edu>
2019-01-25 22:31:02 -0500

    set up basic background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToiletBar from './ToiletParts/ToiletBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+                <ToiletBar/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Textbooks 4 Me
+            hehehe poop
           </p>
         </header>
       </div>
