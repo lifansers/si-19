cb904c53f1ad48b004130d29a810c79715dfe056
yus71 <yus71@pitt.edu>
2019-01-27 00:23:32 -0500

    1

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,64 +1,197 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import MenuIcon from '@material-ui/icons/Menu';
+import DirectionsIcon from '@material-ui/icons/Directions';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
-const styles = {
+import Chip from '@material-ui/core/Chip';
+
+
+const styles = theme => ({
+  appBar: {
+    backgroundColor:'#78909c' ,
+    position: 'relative',
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2,
+  },
+  root: {
+    backgroundColor: '#795548',
+  },
+
   card: {
-    minWidth: 275,
+    maxWidth: 150,
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-};
 
-function SimpleCard(props) {
+    chip: {
+      marginRight: theme.spacing.unit,
+    },
+    section1: {
+      margin: theme.spacing.unit * 2,
+    },
+    section2: {
+      margin: theme.spacing.unit * 4,
+
+    },
+    section3: {
+      margin: `${theme.spacing.unit * 6}px ${theme.spacing.unit * 2}px ${theme.spacing.unit * 2}px`,
+    },
+
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+
+});
+
+
+function Search(props) {
   const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+        <IconButton className={classes.iconButton} aria-label="Menu">
+            <MenuIcon />
+        </IconButton>
+        <Typography variant="h6"  noWrap>
+            Which building are you looking for...
+          </Typography>
+        </Toolbar>
+      </AppBar>
+            <AppBar position="static" className={classes.appBar}>
+              <Toolbar>
+              <IconButton className={classes.iconButton} aria-label="Search">
+                  <MenuIcon />
+              </IconButton>
+              <Typography variant="h6"  noWrap>
+                  Which kind of bathroom are you looking for...
+                </Typography>
+              </Toolbar>
+            </AppBar>
+      <main>
+
+        <div className={classes.root}>
+          <div className={classes.section1}>
+            <Grid container alignItems="center">
+                <Typography gutterBottom variant="h5">
+                  All bathrooms in the area:
+                </Typography>
+
+            </Grid>
+
+          </div>
+          <Divider variant="middle" />
+          <div className={classes.section2}>
+            <Typography gutterBottom variant="h6">
+              Select sorted by
+            </Typography>
+            <div>
+              <Chip className={classes.chip} label="Rate" />
+              <Chip className={classes.chip} label="Location" />
+              <Chip className={classes.chip} label="Cleanliness" />
+              <Chip className={classes.chip} label="Odor" />
+            </div>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="https://cdn4.iconfinder.com/data/icons/objects-things-essentials-vol-2/48/v-49-512.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="William Pitt Union"
+              secondary={
+                <React.Fragment>
+                  <Typography component="span" className={classes.inline} color="textPrimary">
+                    First floor
+                  </Typography>
+                  {" Overall, it is a clean bathroom, however…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+           <ListItem alignItems="flex-start">
+             <ListItemAvatar>
+               <Avatar alt="Remy Sharp" src="https://cdn4.iconfinder.com/data/icons/objects-things-essentials-vol-2/48/v-49-512.png" />
+             </ListItemAvatar>
+             <ListItemText
+               primary="Hillman Library"
+               secondary={
+                 <React.Fragment>
+                   <Typography component="span" className={classes.inline} color="textPrimary">
+                     First floor
+                   </Typography>
+                   {" It is definitely a disgusting bathroom…"}
+                 </React.Fragment>
+               }
+             />
+           </ListItem>
+
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src="https://cdn4.iconfinder.com/data/icons/objects-things-essentials-vol-2/48/v-49-512.png" />
+              </ListItemAvatar>
+              <ListItemText
+                primary="Posvar Hall"
+                secondary={
+                  <React.Fragment>
+                    <Typography component="span" className={classes.inline} color="textPrimary">
+                      Ground floor
+                    </Typography>
+                    {" It is definitely a disgusting bathroom…"}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+
+          </div>
+          <div className={classes.section3}>
+            <Button variant="contained" color="#424242" fullWidth>
+              Write a review
+            </Button>
+          </div>
+        </div>
+      </main>
+    </React.Fragment>
   );
 }
 
-SimpleCard.propTypes = {
+Search.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleCard);
+export default withStyles(styles)(Search);
